166833863fcb7b462ed47102f4b78ef56f8f4379
Soroush Shariaty <soroushshariaty@gmail.com>
2022-01-19 13:58:43 +0330

    searchbar component done

--- components/searchBar/Searchbar.jsx
+++ components/searchBar/Searchbar.jsx
@@ -1,7 +1,20 @@
-import "./searchbar.css";
+import Image from "next/image";
+import logo from "../../public/images/LinkedIn_logo.png";
+import styles from "../../styles/Searchbar.module.css";
+import { AiOutlineSearch } from "react-icons/ai";
 
 const Searchbar = () => {
-  return <div>searchbar</div>;
+  return (
+    <div className={styles.searchbar}>
+      <div className={styles.searchbar__img}>
+        <Image src={logo} alt="logo" />
+      </div>
+      <div className={styles.searchbar__container}>
+        <AiOutlineSearch className={styles.searchbar__container_icon} />
+        <input type="text" placeholder="Search" />
+      </div>
+    </div>
+  );
 };
 
 export default Searchbar;
