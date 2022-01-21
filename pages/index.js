1d9bdd9f04e319167aef01a318e6b5a94c087176
Soroush Shariaty <soroushshariaty@gmail.com>
2022-01-21 23:56:55 +0330

    profile component created in homepage

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Navbar from "./../containers/navbarContainers/navbar/Navbar";
 import BodyContainer from "./../containers/bodyContainer/BodyContainer";
+import Profile from "./../containers/homePageContainers/profile/Profile";
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
       <BodyContainer>
-        <div style={{ backgroundColor: "red", border: "1px solid black" }}>
-          Soroush
+        <div>
+          <Profile />
         </div>
         <div style={{ backgroundColor: "red", border: "1px solid black" }}>
           Soroush
