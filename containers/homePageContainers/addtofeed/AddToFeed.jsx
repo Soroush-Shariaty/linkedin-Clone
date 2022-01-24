0263a040b9c5f2e6996b1b4dabd93b91e97cf1d7
Soroush Shariaty <soroushshariaty@gmail.com>
2022-01-24 20:14:52 +0330

    Small correction on AddToFeed component

--- containers/homePageContainers/addtofeed/AddToFeed.jsx
+++ containers/homePageContainers/addtofeed/AddToFeed.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa";
 import { AddToFeedSuggest } from "./../../../components/homePageComponents/index";
 import styles from "../../../styles/homePageStyles/addToFeed.module.css";
 
@@ -8,6 +10,14 @@ const AddToFeed = () => {
       <AddToFeedSuggest />
       <AddToFeedSuggest />
       <AddToFeedSuggest />
+      <Link href="#" passHref>
+        <a className={styles.addtofeed__recommLink}>
+          <div className={styles.addtofeed__recommLink_container}>
+            <span>View all recommendations</span>
+            <FaArrowRight />
+          </div>
+        </a>
+      </Link>
     </div>
   );
 };
